27e227c37c73de7514f20758a9d76e0c729937ca
kshitij <kshitijchandrakar2019@gmail.com>
2024-04-25 18:45:01 +0530

    aaa

--- Door.gd
+++ Door.gd
@@ -4,6 +4,5 @@ func _input(event):
 	if event is InputEventMouseButton and event.pressed and event.button_index == MOUSE_BUTTON_LEFT:
 		if get_rect().has_point(to_local(event.position)):
 			#add_child(scene)
-			emit_signal("Meow",1)
 			get_tree().change_scene_to_file("res://scene2.tscn")
 
